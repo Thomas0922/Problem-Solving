55e8b7909367d6ba80fd8c141af662be5186bb47
Thomas0922 <126225935+Thomas0922@users.noreply.github.com>
2024-07-03 12:51:16 +0800

    Update 371. Sum of Two Integers.c

--- LeetCode/c/371. Sum of Two Integers.c
+++ LeetCode/c/371. Sum of Two Integers.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 
 int getSum(int a, int b) {
-    unsigned int c=0;
+    unsigned int c=0; //因為要避免溢位所以用umsigned
     while(b){
-        c = a & b;
-        a = a ^ b;
-        b = c << 1;
+        c = a & b; //先算要進位的數在哪
+        a = a ^ b; //算不進位的加法
+        b = c << 1; //左移為了下一輪把進位值加進去
     }return a;
 }
+
+//測試程式
 
 int main(){
     int a,b;
