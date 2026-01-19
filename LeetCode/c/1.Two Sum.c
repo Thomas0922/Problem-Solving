15fe97334533b00e2138e858db93b07d705f57f9
Thomas0922 <126225935+Thomas0922@users.noreply.github.com>
2026-01-19 13:57:20 +0800

    Update 1.Two Sum.c

--- LeetCode/c/1.Two Sum.c
+++ LeetCode/c/1.Two Sum.c
@@ -1,47 +1,78 @@
-#include<stdio.h>
-#include<stdlib.h>
+//原本解法:
 
-
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int *result = (int*)malloc(2 * sizeof(int));
-    *returnSize = 2;
-    for(int i=0;i<numsSize-1;i++){
-        for(int j=i+1;j<numsSize;j++){
-            if(nums[i]+nums[j] == target){
-                result[0]=i;
-                result[1]=j;
-                return result;
+    int *ans = malloc(2 * sizeof(int));
+    if (ans == NULL){
+        *returnSize = 0;
+        return NULL;
+    }
+    for(int i = 0; i < numsSize; i++){
+        for (int j = i+1; j < numsSize; j++){
+            if(nums[i] + nums[j] == target){
+                ans[0] = i;
+                ans[1] = j;
+                *returnSize = 2;
+                return ans;
             }
         }
     }
-    returnSize=0;
-    free(result);
+    free(ans);
+    *returnSize = 0;
     return NULL;
 }
 
-int main(){
-  int numsSize,target;
-  printf("Input Array size:");
-  scanf("%d",&numsSize);
+//更佳解法
+#define HASH_SIZE 10007
 
-  printf("Input target:");
-  scanf("%d",&target);
+typedef struct{
+    int value;
+    int key;
+    int used;
+}HashTable;
 
-  int *nums = malloc(numsSize * sizeof(int));
-
-  printf("Input nums:\n");
-  for(int i=0;i<numsSize;i++){
-    scanf("%d",&nums[i]);
-  }
-
-  int returnSize;
-  int *result = twoSum(nums,numsSize,target,&returnSize);
-  if(result != NULL) {
-        printf("Find Index:[%d,%d]\n", result[0], result[1]);
-        free(result);
-    } else {
-        printf("NULL\n");
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    HashTable* hash = (HashTable*)calloc(HASH_SIZE , sizeof(HashTable));
+    if(hash == NULL){
+        *returnSize = 0;
+        free(hash);
+        return NULL;
     }
-    free(nums);
-    return 0;
+
+    int *ans = (int*)malloc(2 * sizeof(int));
+    if(ans == NULL){
+        *returnSize = 0;
+        free(hash);
+        return NULL;
+    }
+
+    for(int i = 0; i < numsSize; i++){
+        int com = target - nums[i];
+        int h = ((com % HASH_SIZE) + HASH_SIZE) % HASH_SIZE;
+        int idx = h;
+        while(hash[idx].used){
+            if(hash[idx].value == com){
+                ans[0] = hash[idx].key;
+                ans[1] = i;
+                *returnSize = 2;
+                free(hash);
+                return ans;
+            }
+            idx = (idx + 1) % HASH_SIZE;
+        }
+        h = ((nums[i] % HASH_SIZE) + HASH_SIZE) % HASH_SIZE;
+        idx = h;
+        while(hash[idx].used){
+            idx = (idx + 1) % HASH_SIZE;
+        }
+        hash[idx].value = nums[i];
+        hash[idx].key = i;
+        hash[idx].used = 1;
+    }
+    *returnSize = 0;
+    free(hash);
+    free(ans);
+    return NULL;
 }
