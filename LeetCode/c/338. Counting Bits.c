50f58c48f58896f682b159e4600ed1d32540e0c2
Thomas0922 <126225935+Thomas0922@users.noreply.github.com>
2026-01-27 15:01:47 +0800

    Update 338. Counting Bits.c

--- LeetCode/c/338. Counting Bits.c
+++ LeetCode/c/338. Counting Bits.c
@@ -2,18 +2,14 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* countBits(int n, int* returnSize) {
-    int *result = (int*)malloc((n+1) * sizeof(int));
-    *returnSize = n+1;
-    for(int i=0;i<=n;i++){
-        int count = 0;
-        int num = i;
-        while(num){
-         count += num &1;
-         num = num>>1;
-        }
-        result[i] = count;
+    int *ans = (int*)malloc((n + 1) * sizeof(int));
+
+    ans[0] = 0;
+
+    for(int i = 1; i <= n; i++ ){
+        ans[i] = ans[i >> 1] + (i & 1);
     }
-    
-    
-    return result;
+
+    *returnSize = n + 1;
+    return ans;
 }
